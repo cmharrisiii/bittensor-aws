b0fcacbfd5fd16297efcaa5697ebc3cdc274feaf
Jafeel KhanZada <jafeelkhanzada@gmail.com>
2024-01-01 15:15:49 +0500

    fix

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-svm8k0a(g-2#$#5_fplm&v3%*q@cy!uc#avy^^)f^585yxyy@q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'bittensor-api-nlb-ec3983c3dcd9cddb.elb.us-east-1.amazonaws.com'
+]
 
 
 # Application definition
